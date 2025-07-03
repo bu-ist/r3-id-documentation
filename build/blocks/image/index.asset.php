70f94ba38d8b053c208042506a8a24ba7cb7cc51
Tim King <timking@bu.edu>
2025-07-03 14:35:24 -0400

    <image> is sort of working again!

--- build/blocks/image/index.asset.php
+++ build/blocks/image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '3df2d40f13d0da01a457');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '82882e3228858508bca4');
--- build/blocks/tutorial-01/index.asset.php
+++ build/blocks/tutorial-01/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9463a2ea5de2418df3c8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cdc71bb03cfd5f683c94');
