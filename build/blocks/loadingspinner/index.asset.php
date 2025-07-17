afe7663ae75c3d150c30b1f16b1b372cb8d5aef1
Tim King <timking@bu.edu>
2025-07-17 13:19:14 -0400

    Adds GitHub Code Block block

--- build/blocks/loadingspinner/index.asset.php
+++ build/blocks/loadingspinner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '2c38f42733920d851b5f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '120de6f9be80661b163d');
