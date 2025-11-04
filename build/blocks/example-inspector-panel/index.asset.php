43079706ef71ad8409804c64466cf4818438d7ff
Tim King <timking@bu.edu>
2025-11-04 13:37:29 -0500

    fixes the build, which was broken, but is now fixed

--- build/blocks/example-inspector-panel/index.asset.php
+++ build/blocks/example-inspector-panel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'be755212f3b29c93b7bf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'db647ff4d5cd09bf7088');
--- build/blocks/example-static/index.asset.php
+++ build/blocks/example-static/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c74566e58a8f4f3015c5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7427cd695ad489d92d58');
