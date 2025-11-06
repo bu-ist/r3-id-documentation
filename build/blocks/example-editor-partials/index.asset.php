96459a6c1feebb3e0d7dac19f961ba0d8548eb45
Tim King <timking@bu.edu>
2025-11-06 15:56:59 -0500

    rebuild

--- build/blocks/example-editor-partials/index.asset.php
+++ build/blocks/example-editor-partials/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '48df91ca6c50c28375d3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e879ac34563eb59fedba');
